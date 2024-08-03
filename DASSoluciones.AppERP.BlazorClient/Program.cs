0faef1a56ae44fbfdf07958efed005f27bfddb76
Aangelsepulveda <angelsepulvedasep@gmail.com>
2024-08-02 21:41:45 -0400

    instalacion de libreria components

--- DASSoluciones.AppERP.BlazorClient/Program.cs
+++ DASSoluciones.AppERP.BlazorClient/Program.cs
@@ -1,7 +1,10 @@
+using Radzen;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddAppERPServices();
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
